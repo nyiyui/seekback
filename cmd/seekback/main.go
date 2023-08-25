f4ffa4b6c7d5447de398614d98d07476a876c065
nyiyui <+@nyiyui.ca>
2023-08-24 23:25:11 -0400

    symlink to the latest recording

--- cmd/seekback/main.go
+++ cmd/seekback/main.go
@@ -16,10 +16,12 @@ import (
 
 var bufferSize int
 var fileNameTemplate string
+var latestFilename string
 
 func main() {
 	flag.IntVar(&bufferSize, "buffer-size", 20*10000, "buffer size to keep (i.e. audio saved before the dump command) (generally 1000 = 1 second)")
 	flag.StringVar(&fileNameTemplate, "name", "seekback-%s.aiff", "filename template to save recordings to")
+	flag.StringVar(&latestFilename, "latest-name", "seekback-latest.aiff", "filename to put a symlink to the latest recording")
 	flag.Parse()
 	if bufferSize < 0 {
 		log.Fatalf("buffer size must be positive")
@@ -33,7 +35,7 @@ func main() {
 	loop(dumpCh)
 }
 
-func chk(err error) {
+func check(err error) {
 	if err != nil {
 		panic(err)
 	}
@@ -47,7 +49,7 @@ func readRequests(dumpCh chan<- DumpRequest) {
 	r := bufio.NewReader(os.Stdin)
 	for {
 		_, err := r.ReadString('\n')
-		chk(err)
+		check(err)
 		log.Print("dumping")
 		dumpCh <- DumpRequest{
 			Duration: 2 * time.Second,
@@ -56,18 +58,19 @@ func readRequests(dumpCh chan<- DumpRequest) {
 }
 
 func loop(dumpCh <-chan DumpRequest) {
+	// portaudio usage copied from https://github.com/gordonklaus/portaudio/blob/aafa478834f5b0f2ca23dd182b2df227935cb64b/examples/record.go
 	// TODO: maybe make the stream a channel?
 	notify.Notify("READY=1")
-	chk(portaudio.Initialize())
+	check(portaudio.Initialize())
 	defer func() {
-		chk(portaudio.Terminate())
+		check(portaudio.Terminate())
 	}()
 	in := make([]int32, 64)
 	stream, err := portaudio.OpenDefaultStream(1, 0, 44100, len(in), in)
-	chk(err)
+	check(err)
 	defer stream.Close()
 
-	chk(stream.Start())
+	check(stream.Start())
 	buffer := make([][64]int32, bufferSize)
 	bufferIndex := 0
 	bufferStart := 0
@@ -80,7 +83,7 @@ func loop(dumpCh <-chan DumpRequest) {
 			t := time.NewTimer(r.Duration)
 			dump(now, stream, in, buffer[:], bufferStart, bufferIndex, t.C)
 		default:
-			chk(stream.Read())
+			check(stream.Read())
 			buffer[bufferIndex] = [64]int32(in)
 			bufferIndex++
 			if bufferIndex == len(buffer) {
@@ -93,62 +96,64 @@ func loop(dumpCh <-chan DumpRequest) {
 			}
 		}
 	}
-	chk(stream.Stop())
+	check(stream.Stop())
 }
 
 // dataStart and dataEnd are both inclusive.
 func dump[E any](t time.Time, stream *portaudio.Stream, in []int32, data [][64]int32, dataStart, dataEnd int, stop <-chan E) {
+	// TODO: Right now, audio until a message to stop sent is not saved to the buffer. Do we want to have all audio in the buffer?
+	// basically copied from https://github.com/gordonklaus/portaudio/blob/aafa478834f5b0f2ca23dd182b2df227935cb64b/examples/record.go
 	name := fmt.Sprintf(fileNameTemplate, t.Format(time.RFC3339))
 	f, err := os.Create(name)
-	chk(err)
+	check(err)
 
 	// form chunk
 	_, err = f.WriteString("FORM")
-	chk(err)
-	chk(binary.Write(f, binary.BigEndian, int32(0))) //total bytes
+	check(err)
+	check(binary.Write(f, binary.BigEndian, int32(0))) //total bytes
 	_, err = f.WriteString("AIFF")
-	chk(err)
+	check(err)
 
 	// common chunk
 	_, err = f.WriteString("COMM")
-	chk(err)
-	chk(binary.Write(f, binary.BigEndian, int32(18)))                  //size
-	chk(binary.Write(f, binary.BigEndian, int16(1)))                   //channels
-	chk(binary.Write(f, binary.BigEndian, int32(0)))                   //number of samples
-	chk(binary.Write(f, binary.BigEndian, int16(32)))                  //bits per sample
+	check(err)
+	check(binary.Write(f, binary.BigEndian, int32(18)))                //size
+	check(binary.Write(f, binary.BigEndian, int16(1)))                 //channels
+	check(binary.Write(f, binary.BigEndian, int32(0)))                 //number of samples
+	check(binary.Write(f, binary.BigEndian, int16(32)))                //bits per sample
 	_, err = f.Write([]byte{0x40, 0x0e, 0xac, 0x44, 0, 0, 0, 0, 0, 0}) //80-bit sample rate 44100
-	chk(err)
+	check(err)
 
 	// sound chunk
 	_, err = f.WriteString("SSND")
-	chk(err)
-	chk(binary.Write(f, binary.BigEndian, int32(0))) //size
-	chk(binary.Write(f, binary.BigEndian, int32(0))) //offset
-	chk(binary.Write(f, binary.BigEndian, int32(0))) //block
+	check(err)
+	check(binary.Write(f, binary.BigEndian, int32(0))) //size
+	check(binary.Write(f, binary.BigEndian, int32(0))) //offset
+	check(binary.Write(f, binary.BigEndian, int32(0))) //block
 	nSamples := 0
 	defer func() {
 		// fill in missing sizes
 		totalBytes := 4 + 8 + 18 + 8 + 8 + 4*nSamples
 		_, err = f.Seek(4, 0)
-		chk(err)
-		chk(binary.Write(f, binary.BigEndian, int32(totalBytes)))
+		check(err)
+		check(binary.Write(f, binary.BigEndian, int32(totalBytes)))
 		_, err = f.Seek(22, 0)
-		chk(err)
-		chk(binary.Write(f, binary.BigEndian, int32(nSamples)))
+		check(err)
+		check(binary.Write(f, binary.BigEndian, int32(nSamples)))
 		_, err = f.Seek(42, 0)
-		chk(err)
-		chk(binary.Write(f, binary.BigEndian, int32(4*nSamples+8)))
-		chk(f.Close())
+		check(err)
+		check(binary.Write(f, binary.BigEndian, int32(4*nSamples+8)))
+		check(f.Close())
 	}()
 
 	for i := dataStart; i <= dataEnd; i = (i + 1) % len(data) {
-		chk(binary.Write(f, binary.BigEndian, data[i]))
+		check(binary.Write(f, binary.BigEndian, data[i]))
 		nSamples += len(in)
 	}
 Record:
 	for i := 0; true; i++ {
-		chk(stream.Read())
-		chk(binary.Write(f, binary.BigEndian, in))
+		check(stream.Read())
+		check(binary.Write(f, binary.BigEndian, in))
 		nSamples += len(in)
 		select {
 		case <-stop:
@@ -157,4 +162,11 @@ Record:
 		}
 	}
 	log.Printf("Dump complete. Saved to %s", name)
+	writeLatestSymlink(name)
+}
+
+func writeLatestSymlink(name string) {
+	// It's simpler to write remove-then-link than to try making a symlink and then checking for "file already exists" errors.
+	check(os.Remove(latestFilename))
+	check(os.Symlink(name, latestFilename))
 }
